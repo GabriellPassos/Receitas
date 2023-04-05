648a362b531891c9f1f6607f671aa18f76f8994d
GabrielPassos <gabriel_silvapasso@hotmail.com>
2023-04-05 12:03:06 -0300

    refatoração do pivot da fonte do titulo

--- js/responsividade.js
+++ js/responsividade.js
@@ -1,8 +1,27 @@
 const menuHamburguer = document.getElementById("header-fixed-top__btn-menu");
 const headFixedbar = document.querySelector("header");
 var menuOpen = false;
-menuHamburguer.addEventListener('click', evento =>{
+detectar_mobile();
+menuHamburguer.addEventListener('click', evento => {
     menuOpen = !menuOpen;
     headFixedbar.classList.toggle("on", menuOpen);
     document.body.style.overflow = menuOpen ? 'hidden' : 'initial';
 });
+function detectar_mobile() {
+
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        var tituloLogo = document.querySelector(".header-fixed-top-logo__titulo").firstElementChild;
+        tituloLogo.classList.add("correcao-pivot");
+        return true;
+    }
+    else {
+        return false;
+    }
+}
